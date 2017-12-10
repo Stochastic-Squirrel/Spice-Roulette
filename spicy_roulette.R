d82c4fb675bfcb7a9970be5e1253efa085f0cf48
Unknown <strsea001@myuct.ac.za>
2017-12-10 17:43:23 +0200

    Created more exploratory plots, SIMULATION now saves information for each round size and at each game size. Allows to analyse game_size parameter

--- spicy_roulette.R
+++ spicy_roulette.R
@@ -8,11 +8,11 @@ cores <- 8
 cl <- makeCluster(cores)
 registerDoParallel(cl)
 
-max_rounds <- 180
-games <- 1000 # defines how many times you will play spicy roulette with a round size specified above
+max_rounds <- 20
+max_gamesize <- 1000 # defines how many times you will play spicy roulette with a round size specified above
 
 #store for each round size, chances of getting all spicy
-r_results <- data_frame(round_number = numeric(max_rounds), all_spicy_chance = numeric(max_rounds))
+#r_results <- data_frame(round_number = numeric(max_rounds), all_spicy_chance = numeric(max_rounds) , game_size = numeric(max_rounds))
 
 swapping_positions <- function(number,min,max){
   runif(n = number,min,max) %>% round(digits = 0)
@@ -22,8 +22,13 @@ swapping_positions <- function(number,min,max){
 simulation_results <- foreach(round_size = 1:max_rounds, .combine=rbind ,.multicombine = TRUE,.export = c("r_results","swapping_positions")) %dopar%{
   library(tidyverse)
   #define results data frame for each set of games to be played at a given round size
+  #Temporary variable, that stores all_spicy instances
   results <- data_frame(game_index=numeric() , player_1 = numeric()  , all_spicy = logical() )
+  #We also want to see the effect of different game size on the variance of results
   
+  #Variable that each thread will return
+  returning_df <- data_frame(round_number = numeric(max_rounds), all_spicy_chance = numeric(max_rounds) , game_size = numeric(max_rounds)) 
+  for (games in 1:max_gamesize) {
       for (game in 1:games){
         #Define vector of plates
         #A true indicates the spicy burger
@@ -51,19 +56,61 @@ simulation_results <- foreach(round_size = 1:max_rounds, .combine=rbind ,.multic
         results[game,"game_index"] <- game
         results[game,"player_1"] <- sum(plate_1)
         if(sum(plate_1)==3){results[game,"all_spicy"] = TRUE }else{results[game,"all_spicy"] = FALSE }
-        
-        
       }
-  #once we have finsihed all games at a given round size, record % chance of getting all spicy
- c(round_size ,
- (sum(results$all_spicy)/dim(results)[1])*100 )
+    #once we have finsihed all games at a given round size, record % chance of getting all spicy
+    #Add results to df that will be returned
+    returning_df <- bind_rows(
+      returning_df,
+      data_frame(round_number = round_size , all_spicy_chance = (sum(results$all_spicy)/dim(results)[1])*100  , game_size = games) 
+    )
+    # c(round_size ,
+    #   (sum(results$all_spicy)/dim(results)[1])*100 , games )
+    }
+    returning_df
 }
 
 stopCluster(cl)
 
 #put results into a dataframe
-r_results$round_number <- simulation_results[1:max_rounds,1] 
-r_results$all_spicy_chance <- simulation_results[1:max_rounds,2]
-saveRDS(r_results ,"naive_simulation.rds")
-simulation_plot <- ggplot(r_results) + aes(x = round_number , y = all_spicy_chance) + geom_point() + geom_smooth(col="red",method="lm")
-saveRDS(simulation_plot,"simulation_plot.rds")
+#r_results$round_number <- simulation_results[1:max_rounds,1] 
+#r_results$all_spicy_chance <- simulation_results[1:max_rounds,2]
+
+#Exclude 0 game size entries. I think they are created as a result of the multithreading
+simulation_results <- simulation_results %>% filter(game_size != 0)
+saveRDS(simulation_results ,"results/naive_simulation.rds")
+
+#Define results for plotting
+
+plotting_with_outliers <- simulation_results #include game sizes from 3 upwards
+simulation_plot_with_outliers <- ggplot(plotting_with_outliers) + aes(x = factor(round_number) , y = all_spicy_chance , col = game_size) + scale_color_gradient(low="green",high="red") +
+                  xlab("Round Size") + ylab("% of games where one player had all 3 spicy burgers") + labs(title ="Results with outliers",color ="Game Size") + geom_point(size=5)  
+simulation_plot_with_outliers
+saveRDS(simulation_plot_with_outliers,"plots/simulation_plot_with_outliers.rds")
+
+
+
+plotting_without_outliers <- simulation_results %>% filter(game_size > 3) #include game sizes from 3 upwards
+simulation_plot_without_outliers <- ggplot(plotting_without_outliers) + aes(x = factor(round_number) , y = all_spicy_chance , col = game_size) + scale_color_gradient(low="green",high="red") +
+  xlab("Round Size") + ylab("% of games where one player had all 3 spicy burgers") + labs(title ="Results without extreme outliers",color ="Game Size") + geom_point(size =5)
+simulation_plot_without_outliers
+saveRDS(simulation_plot_without_outliers,"plots/simulation_plot_without_outliers.rds")
+
+simulation_box_plot <- ggplot(plotting_without_outliers) + aes(x = factor(round_number) , y = all_spicy_chance )+  scale_y_continuous(breaks = round(seq(min(plotting_without_outliers$all_spicy_chance), max(plotting_without_outliers$all_spicy_chance), by = 1),1)) +
+  xlab("Round Size") + ylab("% of games where one player had all 3 spicy burgers") + labs(title ="Boxplot of results without extreme outliers") +geom_boxplot()
+simulation_box_plot
+saveRDS(simulation_box_plot,"plots/simulation_box_plot.rds")
+
+
+#Plot of round_number vs all_spicy chance for ALL game sizes
+line_plot_overall <- ggplot(plotting_without_outliers) + aes(x = round_number , y = all_spicy_chance , col = game_size) + geom_smooth()
+line_plot_overall
+
+
+#For a given game size, how will round number affect
+
+
+line_gamesize_round_number <- ggplot(plotting_without_outliers) + aes(x = game_size , y = all_spicy_chance) + geom_smooth() +facet_wrap(~ round_number , ncol =6)
+line_gamesize_round_number
+
+
+#After what game size do things stablillise?
